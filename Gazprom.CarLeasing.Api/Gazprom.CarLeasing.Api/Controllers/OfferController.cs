2cbe84c867a82e48b3c5c8fadd8fdbb138fdd805
Соколов Артём <asokolov@softrust.ru>
2025-07-14 23:12:43 +0300

    added api layer

---
+++ Gazprom.CarLeasing.Api/Gazprom.CarLeasing.Api/Controllers/OfferController.cs
@@ -0,0 +1,29 @@
+﻿using Gazprom.CarLeasing.Application.Requests;
+using Gazprom.CarLeasing.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gazprom.CarLeasing.Api.Controllers;
+
+/// <summary>
+/// Контроллер для работы с оффером.
+/// </summary>
+/// <param name="offerService">Сервис для работы с оффером.</param>
+[ApiController]
+[Route("api/[controller]")]
+public class OfferController(IOfferService offerService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreatOfferAsync(
+        CreateOfferRequest request,
+        CancellationToken cancellationToken = default) =>
+         StatusCode(
+             StatusCodes.Status201Created,
+             await offerService.CreateOfferAsync(request, cancellationToken));
+
+    [HttpGet("view")]
+    public async Task<IActionResult> GetOfferViewAsync(
+        OfferViewFilter filter,
+        CancellationToken cancellationToken = default) =>
+        Ok(await offerService.GetOfferViewAsync(filter, cancellationToken));
+
+}
---
+++ Gazprom.CarLeasing.Api/Gazprom.CarLeasing.Api/Controllers/SupplierController.cs
@@ -0,0 +1,18 @@
+﻿using Gazprom.CarLeasing.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gazprom.CarLeasing.Api.Controllers;
+
+/// <summary>
+/// Контроллер для работы с поставщиком.
+/// </summary>
+/// <param name="supplierService">Сервис для работы с поставщиком.</param>
+[ApiController]
+[Route("api/[controller]")]
+public class SupplierController(ISupplierService supplierService) : ControllerBase
+{
+    [HttpGet("top")]
+    public async Task<IActionResult> GetTopSuppliersAsync(
+        CancellationToken cancellationToken = default) =>
+        Ok(await supplierService.GetTopSuppliersAsync(cancellationToken));
+}
